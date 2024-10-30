7548809eb54f6bac9ed26d50b7517518793aedb9
Giovani V <g.valenciab@uniandes.edu.co>
2024-10-30 09:38:21 -0500

    final

--- src/logica/Parcial.java
+++ src/logica/Parcial.java
@@ -7,8 +7,8 @@ public class Parcial {
 	public double[] hallarRaices(double a, double b, double c) throws Exception {
 	 //ax^2 + bx + c = 0
 	//ArrayList<Double> actividadesExistentes = new ArrayList<Double>();
-	double calculoP = -b+(Math.sqrt(Math.pow(b, 2)) -(4*a*c))/2*a;
-	double calculoN = -b-(Math.sqrt(Math.pow(b, 2)) -(4*a*c))/2*a;
+	double calculoP = (-b+(Math.sqrt(Math.pow(b, 2) -(4*a*c))))/(2*a);
+	double calculoN = (-b-(Math.sqrt(Math.pow(b, 2) -(4*a*c))))/(2*a);
 	double discriminante = Math.pow(b, 2)-(4*a*c);
 	if (discriminante == 0){
 		double[] unaRespuesta = {calculoP, -1};
